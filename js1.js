9932a5c35d57303c8fadd5c2eefedce724eb52c7
Dork <imnotaprogrammer@protonmail.com>
2020-05-28 18:21:52 -0500

    It already works!

--- js1.js
+++ js1.js
@@ -1,41 +1,44 @@
 let btn = document.querySelectorAll('.btn');
+let ce = document.querySelector('#ce');
 let output = document.querySelector('#out');
 let eq = document.querySelector('#eq');
-let adas = '+';
+let mathOp = '+';
 
 btn.forEach((element) => {
     element.addEventListener('click', () => {
         let values = element.dataset.value;
         if (!values.includes(' = ')) {
-            adas += String(element.dataset.value);
+            mathOp += String(element.dataset.value);
         }
-        // if (classes.includes('op')) console.log('hi');
         output.textContent += element.textContent;
     })
 })
 
 function getResult() {
-    output.textContent = adas;
-    let temp = adas.split(' +');
+    let temp = mathOp.split(' +');
     let result = 0;
     temp.forEach((element) => {
         let alpha;
         switch (element[0]) {
             case '*':
                 alpha = element.replace(/\*/g, '');
-                result = result * alpha;
-                output.textContent = result;
+                result *= alpha;
                 break;
             case '/':
                 alpha = element.replace(/\//g, '');
-                result = result / alpha;
-                output.textContent = result;
+                result /= alpha;
                 break;
             default:
                 result += Number(element);
-                output.textContent = result;
         }
     })
+    output.textContent = result;
 }
 
-eq.addEventListener('click', getResult)
+function clearOutput() {
+    output.textContent = '';
+    mathOp = 0;
+}
+
+eq.addEventListener('click', getResult);
+ce.addEventListener('click', clearOutput)
